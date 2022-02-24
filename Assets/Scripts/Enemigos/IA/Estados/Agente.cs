df97419c77cc3a0a2ef16778fd12099d3ee5d622
JDSucunza <jdsucunza@gmail.com>
2022-02-24 17:56:36 -0300

    juego

---
+++ Assets/Scripts/Enemigos/IA/Estados/Agente.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agente : MonoBehaviour
+{
+    [HideInInspector]
+    public NavMeshAgent nma;
+
+    [HideInInspector]
+    public Animator anim;
+    // Lista de Estados:
+    [HideInInspector]
+    public Idle estadoIdle;
+    [HideInInspector]
+    public Recorrer estadoRecorrer;
+    [HideInInspector]
+    public Detectar estadoDetectar;
+    //--------------------------------------------------------------------------------------------
+    public GameObject player;
+    
+    private void Awake (){
+        estadoIdle = GetComponent <Idle>();
+        estadoRecorrer = GetComponent <Recorrer>();
+        estadoDetectar = GetComponent <Detectar> ();
+        nma = GetComponent <NavMeshAgent> ();
+        anim = GetComponent <Animator> ();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/P.Principal/Disparo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+     public float rayDistance = 20f;
+    public LayerMask layers;
+    public float cooldown = 0.5f;
+    public float _cooldown;
+
+    private void Update(){
+        if(Input.GetButtonDown("Fire1") && this._cooldown <= 0){
+            this._cooldown = this.cooldown;
+            RaycastHit hit;
+            bool choco = Physics.Raycast(Camera.main.transform.position, this.transform.forward, out hit, this.rayDistance, this.layers);
+            Debug.DrawRay(this.transform.position, this.transform.forward * this.rayDistance, Color.red, duration: 2f);
+            if(choco){
+                if(hit.collider.CompareTag("Enemy")){
+                    Debug.Log("Choque contra un enemigo");
+                }
+                if(hit.collider.CompareTag("Wall")){
+                    Debug.Log("Choque contra una pared");
+                }
+            }
+        } else {
+            this._cooldown -= 1 * Time.deltaTime;
+        }
+    }
+}
